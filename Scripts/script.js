97f1b2e017194edbb21663f587d66773c311ce2e
La1nnik <159943235+La1nnik@users.noreply.github.com>
2025-09-26 19:57:09 +0100

    Initial clean commit

---
+++ Scripts/script.js
@@ -0,0 +1,79 @@
+const { ipcRenderer } = require("electron");
+
+const stateBtn = document.getElementById("state");
+const clicksHTML = document.getElementById("clicks");
+const clickInterval = document.getElementById("clickInterval");
+
+
+//sends the input of click interval from the renderer to the main proccess;
+function sendValue() {
+    ipcRenderer.send("interval", Number(clickInterval.value));
+}
+
+
+
+
+
+//Stopwatch==================================================
+
+let output = document.getElementById("runtime");
+let time;
+let sec = 0;
+let min = 0;
+let hr = 0;
+
+function timer() {
+    sec++;
+    if (sec === 60) {
+        min++
+        sec = 0
+    }
+    if (min === 60) {
+        hr++;
+        sec, min = 0;
+    }
+
+    //Doing some string interpolation
+
+    let seconds = sec < 10 ? `0` + sec : sec;
+    let minute = min < 10 ? `0` + min : min;
+    let hour = hr < 10 ? "0" + hr : hr;
+
+    let timer = `${hour}:${minute}:${seconds}`;
+    output.innerHTML = timer;
+};
+//===========================================================
+
+
+
+
+
+
+
+
+
+//AutoClicker ON
+ipcRenderer.on("started", () => {
+
+    time = setInterval(timer, 1000);
+
+    clicksHTML.style.color = "#a551f5"
+    stateBtn.innerHTML = "Active";
+    stateBtn.style.color = "#f03d3dff";
+
+
+});
+
+//AutoClicker OFF
+ipcRenderer.on("ended", () => {
+    clearInterval(time);
+    stateBtn.innerHTML = "Idle";
+    stateBtn.style.color = "#78ff84";
+
+});
+
+//ClickCount
+
+ipcRenderer.on("clickCount", (event, clickCount) => {
+    clicksHTML.innerHTML = clickCount;
+});
